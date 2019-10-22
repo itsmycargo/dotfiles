2357ae4220f160eccdf4ee06538d9712dd9e97d0
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2019-10-22 22:28:58 +0200

    feat: Add TouchID support

---
+++ post-update.d/touchid.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+if ! grep -q 'pam_tid.so' /etc/pam.d/sudo;
+then
+  sudo perl -pi -e '$_ .= qq(auth       sufficient     pam_tid.so\n) if /auth       sufficient     pam_smartcard.so/' /etc/pam.d/sudo
+fi
