f5463bcef4eaa403faa686b2210e4397a59661b9
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2019-10-22 22:38:05 +0200

    fix: display status for post-update

--- post-update.d/touchid.sh
+++ post-update.d/touchid.sh
@@ -4,5 +4,6 @@ set -e
 
 if ! grep -q 'pam_tid.so' /etc/pam.d/sudo;
 then
+  echo "--> Enabling TouchID for sudo"
   sudo perl -pi -e '$_ .= qq(auth       sufficient     pam_tid.so\n) if /auth       sufficient     pam_smartcard.so/' /etc/pam.d/sudo
 fi
