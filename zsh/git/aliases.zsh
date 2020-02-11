8e03ebe481e9a8c394bee3c29da6e920da6377e3
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2020-02-11 11:45:21 +0100

    fix: broken delete-squashed

--- zsh/git/aliases.zsh
+++ zsh/git/aliases.zsh
@@ -2,13 +2,16 @@ function git-delete-squashed () {
   OVERCOMMIT_DISABLE=1 git checkout -q master
   git fetch
   git pull
-  
+
   while read branch;
   do
     mergeBase=$(git merge-base master $branch)
-    if [[ $(git cherry master $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergeBase -m _)) == "-"* ]];
+    if [ -n "$mergeBase" ];
     then
-      git branch -D $branch
+      if [[ $(git cherry master $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergeBase -m _)) == "-"* ]];
+      then
+        git branch -D $branch
+      fi
     fi
   done < <(git for-each-ref refs/heads/ "--format=%(refname:short)")
 }
