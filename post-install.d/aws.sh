127706974052a4a12606a68c9a66fcefe125da73
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2020-09-16 15:22:47 +0200

    feat: support ecr-login for docker

--- post-install.d/aws.sh
+++ post-install.d/aws.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 if [ -z "$(/usr/local/bin/aws configure get aws_access_key_id)" ]; then
   echo "AWS Credentials"
   echo "Visit https://console.aws.amazon.com/iam/home?region=eu-central-1#/security_credentials"
@@ -10,4 +12,21 @@ if [ -z "$(/usr/local/bin/aws configure get aws_access_key_id)" ]; then
   /usr/local/bin/aws configure set aws_access_key_id "${aws_access_key_id}"
   /usr/local/bin/aws configure set aws_secret_access_key "${aws_secret_access_key}"
   /usr/local/bin/aws configure set default.region eu-central-1
+fi
+
+# Ensure Docker Credentials
+if ! grep -q "ecr-login" ~/.docker/config.json;
+then
+  mkdir -p ~/.docker
+  if [ ! -e ~/.docker/config.json ];
+  then
+    echo '{}' > ~/.docker/config.json
+  fi
+
+  /usr/local/bin/brew install jq docker-credential-helper-ecr
+
+  jq \
+    '. + {"credsHelpers": {"003688427525.dkr.ecr.eu-central-1.amazonaws.com": "ecr-login"}}' \
+    ~/.docker/config.json > ".$$"
+  mv ".$$" ~/.docker/config.json
 fi
