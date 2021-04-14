7f6e7c01419554ebd6c8515d8716274e45bb5c0e
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2021-04-14 20:02:10 +0200

    Better git-delete-squashed

--- zsh/git/aliases.zsh
+++ zsh/git/aliases.zsh
@@ -1,14 +1,21 @@
 function git-delete-squashed () {
-  OVERCOMMIT_DISABLE=1 git checkout -q master
+  if [ "$(git_branch)" != "master" -a "$(git_branch)" != "main" ];
+  then
+    echo "Please run git-delete-squashed on main branch (either master or main)"
+    return 1
+  fi
+
+  base_branch=$(git_branch)
+
   git fetch
   git pull
 
   while read branch;
   do
-    mergeBase=$(git merge-base master $branch)
+    mergeBase=$(git merge-base ${base_branch} $branch)
     if [ -n "$mergeBase" ];
     then
-      if [[ $(git cherry master $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergeBase -m _)) == "-"* ]];
+      if [[ $(git cherry ${base_branch} $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergeBase -m _)) == "-"* ]];
       then
         git branch -D $branch
       fi
